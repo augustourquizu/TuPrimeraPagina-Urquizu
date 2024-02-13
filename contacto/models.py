becce16427ee9de3ea73aa45f49dc8ec13c1955b
AugustoUrquizu <augusto.urquizu@gmail.com>
2024-02-13 18:55:30 -0300

    Terminado

--- contacto/models.py
+++ contacto/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Contacto(models.Model):
     def __str__(self) -> str:
-        return f' Nombre: {self.nombre} {self.apellido} Telefonno:{self.telefono} Mail:{self.mail}'
+        return f' Nombre: {self.nombre} {self.apellido} Telefono:{self.telefono} Mail:{self.mail}'
     nombre=models.CharField(max_length=20)
     apellido=models.CharField(max_length=20)
     telefono=models.IntegerField()
